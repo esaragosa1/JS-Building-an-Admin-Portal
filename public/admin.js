dae2d584884aab51a03b11a41da6b231ebf6f552
esaragosa1 <108439543+esaragosa1@users.noreply.github.com>
2022-10-03 16:05:31 -0700

    followed study
    forgot to make comments

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,46 @@
+let addButton = document.querySelector('#add-book')
 
-// Your Code Here
+
+async function assignId () {
+  let newId = 0;
+  let response = await fetch('http://localhost:3001/listBooks');
+  
+  let data = await response.json();
+  
+  data.forEach(book => {
+    if(book.id > newId) {
+      newId = book.id
+    }
+  })
+return newId + 1;
+}
+  
+addButton.addEventListener('click', async () => {
+
+  let id = await assignId();
+
+  let dataToSend = {
+    id: id,
+    title: document.querySelector('#add-title').value,
+    year: document.querySelector('#add-year').value,
+    description: document.querySelector('#add-description').value,
+    quantity: document.querySelector('#add-quantity').value,
+    imageURL: document.querySelector('#add-imageURL').value,
+  }  
+
+  fetch('http://localhost:3001/addBook',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dataToSend)
+  })
+
+})
+
+ 
+
+  
+ 
+
+ 
